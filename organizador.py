bb3a148e461ae1d3ead73ea0e47806056c84184b
AskepiosV <askepiosgames@gmail.com>
2026-04-29 15:46:15 -0500

    Solucion a problemas

--- organizador.py
+++ organizador.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 extenciones = {
+    ".png": "Imagenes",
     ".jpg": "Imagenes",
     ".jpeg": "Imagenes",
     ".docx": "Documentos",
@@ -13,16 +14,19 @@ extenciones = {
 ruta = Path.home() / "Descargas"
 
 for i in ruta.iterdir():
-    categoria = extenciones.get(i.suffix, "Sin Categoria")
+    if i.is_dir():
+        continue
+    categoria = extenciones.get(i.suffix.lower(), "Sin Categoria")
     destino = ruta / categoria 
     destino.mkdir(exist_ok =True)
-    i.rename (destino / i.name)
+    rutaFinal = destino / i.name
+    count = 1 
+    while rutaFinal.exists():
+        nuevoNombre = (i.stem + str(count) + i.suffix ) 
+        rutaFinal = destino / nuevoNombre
+        count += 1 
+        
+    i.rename (rutaFinal)
 
 print ("Todo ordenado... Askepios")
-
-
-
-
-
-
 
